3e2ab7af6beaa331f3f2431edcef6d0981195583
BootStorm <james@forrestclan.com>
2015-09-30 23:59:03 +0000

    hashadmins exercise

--- modules/system/manifests/hashadmins.pp
+++ modules/system/manifests/hashadmins.pp
@@ -1,20 +1,24 @@
 class system::hashadmins {
   require mysql::server
 
-  Mysql_user {
+  $mysql_defaults = {
     ensure        => present,
     password_hash => mysql_password('puppetlabs'),
   }
 
-  mysql_user { ['zack@localhost', 'monica@localhost', 'luke@localhost']: 
+  $mysql_users = {
+    'zack@localhost'   => {},
+    'monica@localhost' => {},
+    'luke@localhost'   => {},
+    'ralph@localhost'  => {
+      ensure => absent,
+    },
+    'brad@localhost'   => {
+      password_hash => mysql_password('puppet'),
+    },
   }
 
-  mysql_user { 'ralph@localhost':
-    ensure => absent,
-  }
+  create_resources('mysql_user', $mysql_users, $mysql_defaults) 
 
-  mysql_user { 'brad@localhost':
-    password_hash => mysql_password('puppet'),
-  }
 }
 
