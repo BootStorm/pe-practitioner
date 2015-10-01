08c6b77f5ec5d758f089a3bb2ef4b413dac34c1d
BootStorm <james@forrestclan.com>
2015-10-01 03:24:04 +0000

    exporting a host resource

---
+++ modules/hosts/manifests/init.pp
@@ -0,0 +1,29 @@
+class hosts {
+
+  Host {
+    ensure => present,
+    target => '/etc/hosts',
+  }
+
+  host { 'localhost':
+    host_aliases => ['localhost.localdomain', 'localhost4', 'localhost4.localdomain4'],
+    ip           => '127.0.0.1',
+  }
+
+  @@host { $hostname:
+    ip  => $ipaddress,
+    tag => 'classroom',
+  }
+  
+  host { 'pe-server':
+    host_aliases => ['puppet'],
+    ip           => '192.168.1.160',
+  }
+
+  Host <<| tag == 'classroom' |>>
+
+  resources { 'host':
+    purge => true,
+  }
+
+}
