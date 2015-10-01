8389ab2ef519c0fef100c495de1b923e8d962b87
BootStorm <james@forrestclan.com>
2015-10-01 00:40:50 +0000

    added the website admins using resource collectors and realizations

---
+++ modules/system/manifests/admins.pp
@@ -0,0 +1,16 @@
+class system::admins {
+
+  User {
+    ensure => present,
+  }
+
+  @user { ['bob', 'sally']:
+    tag     => 'wordpress',
+  }
+
+  @user { 'joe':
+    tag => 'oracle',
+  }
+
+  @user { 'admin': }
+}
---
+++ modules/website/manifests/admins.pp
@@ -0,0 +1,10 @@
+class website::admins {
+
+  include system::admins
+
+  User <| tag == 'wordpress' |>
+
+  realize User['admin']
+
+}
+
