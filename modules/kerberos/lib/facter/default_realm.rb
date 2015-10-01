d2e966bc8fc9022b6edf45811c8f295367514c08
BootStorm <james@forrestclan.com>
2015-10-01 06:14:36 +0000

    default realm with augeas and extra credit

--- modules/kerberos/lib/facter/default_realm.rb
+++ modules/kerberos/lib/facter/default_realm.rb
@@ -1,5 +1,12 @@
 #default_realm.rb
+
 Facter.add('default_realm') do
-  setcode 'awk \'/default_realm/ {print $NF}\' /etc/krb5.conf'
+  #setcode 'awk \'/default_realm/ {print $NF}\' /etc/krb5.conf'
+  setcode do
+    require 'augeas'
+    Augeas::open do |aug|
+      aug.get('/files/etc/krb5.con/libdefaults/default_realm')
+    end
+  end
 end
 
---
+++ modules/kerberos/manifests/init.pp
@@ -0,0 +1,9 @@
+class kerberos {
+
+  augeas { 'krb5.conf':
+    context => '/files/etc/krb5.conf/libdefaults',
+    changes => 'set default_realm PUPPETLABS.VM',
+  }
+
+}
+
