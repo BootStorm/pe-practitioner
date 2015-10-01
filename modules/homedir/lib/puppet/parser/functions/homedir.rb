31046ffcf45affaa3dc7eb96b2de93b7d93815e0
BootStorm <james@forrestclan.com>
2015-10-01 04:57:32 +0000

    added homedir functions

---
+++ modules/homedir/lib/puppet/parser/functions/homedir.rb
@@ -0,0 +1,18 @@
+# homedir.rb
+module Puppet::Parser::Functions
+  newfunction(:homedir,
+              :type => :rvalue,
+              :arity => 1
+             )
+  do |args|
+    username = args[0]
+    case username
+    when 'root'
+      return 'root'
+    else
+      return "/home/#{username}"
+    end
+  end
+end
+
+
---
+++ modules/homedir/tests/homedir.pp
@@ -0,0 +1,10 @@
+class homedir::homedir {
+
+  $roothome = homedir('root')
+  $testhome = homedir('test')
+
+  notify { "Root's homedir is ${roothome}": }
+  notify { "Test's homedir is ${testhome}": }
+
+}
+
